113e2ae71b2991e141e9d6524d36832e4fc433f6
Federico Tempone <fedetempone5@gmail.com>
2025-09-25 18:53:30 -0300

    debbuging errors

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://genhr.onrender.com", 
+}));
 app.use(express.json());
 
 // API de contacto
